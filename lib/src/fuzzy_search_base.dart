979c39a922c1725cbc4afd5f4c98b11b8e699272
Vishesh Handa <me@vhanda.in>
2021-04-26 09:50:52 +0200

    First implementation

--- lib/src/fuzzy_search_base.dart
+++ lib/src/fuzzy_search_base.dart
@@ -1,3 +1,17 @@
-bool fuzzySearch(String base, String s) {
+bool fuzzySearch(String base, String needle) {
+  if (needle.isEmpty) {
+    return true;
+  }
+
+  var remainder = String.fromCharCodes(needle.codeUnits);
+  for (var char in base.codeUnits) {
+    if (char == remainder.codeUnits.first) {
+      remainder = String.fromCharCodes(remainder.codeUnits, 1);
+      if (remainder.isEmpty) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
